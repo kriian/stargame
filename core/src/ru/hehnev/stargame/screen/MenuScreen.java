94395b5507da358388632bdde47f53294449f37a
maniana <manianahek@gmail.com>
2021-08-30 14:37:21 +0300

    Lesson 2

---
+++ core/src/ru/hehnev/stargame/screen/MenuScreen.java
@@ -0,0 +1,78 @@
+package ru.hehnev.stargame.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.hehnev.stargame.base.BaseScreen;
+
+public class MenuScreen extends BaseScreen {
+
+    private Texture img;
+    private Vector2 pos;
+    private Vector2 touch;
+    private Vector2 v;
+
+    @Override
+    public void show() {
+        super.show();
+        img = new Texture("badlogic.jpg");
+        pos = new Vector2();
+        touch = new Vector2();
+        v = new Vector2();
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        batch.begin();
+        batch.draw(img, pos.x, pos.y);
+        batch.end();
+        pos.add(v);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        pos.set(touch);
+        return super.touchDown(screenX, screenY, pointer, button);
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        pos.set(touch);
+        return super.touchDragged(screenX, screenY, pointer);
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        switch (keycode) {
+            case Input.Keys.UP:
+                v.set(0, 5);
+                break;
+            case Input.Keys.DOWN:
+                v.set(0, -5);
+                break;
+            case Input.Keys.LEFT:
+                v.set(-5, 0);
+                break;
+            case Input.Keys.RIGHT:
+                v.set(5, 0);
+                break;
+        }
+        return super.keyDown(keycode);
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        v.setZero();
+        return super.keyUp(keycode);
+    }
+}
