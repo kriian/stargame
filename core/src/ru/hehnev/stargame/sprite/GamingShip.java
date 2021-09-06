c027bfb2fc029e8429db8a71dca7aaec6ea2b2ff
maniana <manianahek@gmail.com>
2021-09-06 14:37:19 +0300

    lesson 4 task 2

--- core/src/ru/hehnev/stargame/sprite/GamingShip.java
+++ core/src/ru/hehnev/stargame/sprite/GamingShip.java
@@ -1,6 +1,7 @@
 package ru.hehnev.stargame.sprite;
 
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.hehnev.stargame.base.Sprite;
@@ -11,11 +12,11 @@ public class GamingShip extends Sprite {
     private static final float HEIGHT = 0.2f;
 
     private final Vector2 v;
-
+//    private final TextureRegion region;
 
 
     public GamingShip(TextureAtlas atlas) {
-        super(atlas.findRegion("main_ship"));
+        super(atlas.findRegion("main_ship").split(390 / 2, 287)[0][0]);
         v = new Vector2();
     }
 
